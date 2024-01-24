15a11c719feac7d6773669413455e7666ac4a897
manucarrete <146010216+manucarrete@users.noreply.github.com>
2024-01-24 16:20:30 +0000

    Personajes2

--- esdla/src/app/app.component.ts
+++ esdla/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonajeComponent } from './personaje/personaje.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PersonajeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
